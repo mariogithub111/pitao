315fd09f10c028b903c2c10081c7eeb9fb6fcbbe
Filipa Andrade <fandrade@laterpay.net>
2023-12-05 19:01:01 +0100

    Adiciona funcao para encontrar o maior elemento de uma lista

--- lista.py
+++ lista.py
@@ -4,18 +4,27 @@ impares = [1, 3, 5, 7, 9, 11]
 
 # Fazer uma função que receba uma lista de inteiros e devolva a soma dos seus elementos
 def soma(lista):
-    soma = 0
+    sum = 0
     # Exemplo: [4, 6, 2, 3, 9, 1, 10]
     for elemento in lista:  # primeira vez: 4, segunda vez: 6, ...
-        soma = soma + elemento  # primeira vez: 0 + 4 = 4, segunda vez: 4 + 6 = 10, ...
+        sum = sum + elemento  # primeira vez: 0 + 4 = 4, segunda vez: 4 + 6 = 10, ...
 
-    return soma
-
+    return sum
 
 print(soma(pares))
 print(soma(impares))
 print(soma([4, 6, 2, 3, 9, 1, 10]))
 
-
 # Exercicio 2:
 # Fazer uma função que receba uma lista de inteiros e devolva o maior dos seus elementos
+def maior(lista):
+    max = 0  # Assumimos que o maximo e 0
+    for elemento in lista:  # Vamos percorrer a lista
+        if elemento > max:  # Se o elemento em que estamos e maior que o maximo
+            max = elemento  # Atualizamos o maximo
+
+    return max  # Retornamos o maximo
+
+print(maior(pares))
+print(maior(impares))
+print(maior([3, 7, 11, 1, 8, 5, 19, 2, 0, 5, 6, 17]))
